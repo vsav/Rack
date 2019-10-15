cbf0794210e2e6490f42bd3f5938c24359093c1b
Vadim Savostin <vsavdev@gmail.com>
2019-10-15 19:02:11 +0300

    create app.rb

--- app.rb
+++ app.rb
@@ -1,26 +1,41 @@
+require_relative 'time_output'
+
 class App
 
   def call(env)
-    perform_request
-    [status, headers, body]
+    @request = Rack::Request.new(env)
+    @path = @request.path
+    @formats = @request.params['format']
+    check_request
   end
 
   private
-
-  def perform_request
-    sleep rand(2..3)
-  end
-
-  def status
-    200
-  end
 
   def headers
     { 'Content-Type' => 'text/plain' }
   end
 
-  def body
-    ["Welcome aboard!\n"]
+  def check_request
+    if @path != '/time'
+      [404, headers, ["Error: Invalid URL\n"]]
+    elsif @formats.nil?
+      [400, headers, ["Error: Time formats missing\n"]]
+    else
+      convert_time
+    end
+  end
+
+  def convert_time
+    output = TimeOutput.new(@formats)
+    if output.formats_valid?
+      status = 200
+      body = output.converted_time
+    else
+      status = 400
+      body = "Error: Unknown format(s) #{output.unknown_time_formats}
+      Allowed time formats: #{TimeOutput::FORMATS_ALLOWED.keys}"
+    end
+    [status, headers, ["#{body}\n"]]
   end
 
 end
